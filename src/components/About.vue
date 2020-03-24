b014f0115b1bfea7046a015323863c4a33e272f5
Antonio Pontes <anvictorsantos@gmail.com>
2020-03-23 22:50:24 -0300

    new - first commit

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="about">
+        <h1>Hello, {{ their_name }}</h1>
+        <h1>This is an about page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Skills',
+    data() {
+        return {
+            their_name: this.$route.params.name
+        }
+    }
+}
+</script>
